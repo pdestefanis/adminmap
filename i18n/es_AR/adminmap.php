4daf76464452c876256bc4aab834522b4fc4ae2b
Pablo Destefanis <pdestefanis@gmail.com>
2011-12-02 14:45:08 -0500

    Added Spanish translation

---
+++ i18n/es_AR/adminmap.php
@@ -0,0 +1,62 @@
+<?php
+/**
+ *  Admin Map English Language file
+ *
+ * This plugin was written for Ushahidi Liberia, by the contractors of Ushahidi Liberia
+ * 2011
+ *
+ * @package    Admin Map plugin
+ * @author     Carter Draper <carjimdra@gmail.com>
+ * @author     Kpetermeni Siakor <tksiakor@gmail.com>
+ * @translator Pablo Destefanis <pdestefanis@gmail.com>
+ */ 
+
+	$lang = array(
+	'admin_map_main_menu_tab' => 'Mapa',
+	'big_map_main_menu_tab' => 'Agrandar Mapa',
+	'boolean_operators' => 'Operadores Lógicos:',
+	'FILTERS' => 'FILTROS',
+	'Categories' => 'Categorías',
+	'TIME_LINE' => 'LINEA DE TIEMPO',
+	'ORHEADER'  => 'OR',
+	'ORBODY' =>				'El operador lógico OR permite ver os reportes pertenecientes a cualquiera de las categorías seleccionadas.
+							<br/><br/>
+							Por ejemplo, si ha selecccionado categorías A, B, y C, entonces vera todos los reportes que hayan pertenezcan
+							a las categorías A <strong>o</strong> B <strong>o</strong> C. Algunos de los reportes que se muestran 
+                            pertenecerán sólo a la categoría A, otros solo a la categoría C. Algunes pueden pertenecer a ambas 
+                            categorías .<br/><br/>
+							Cuando se seleccione este operador, los marcadores se pintarán de acuerdo a la categoría seleccionada.
+							Por ejemplo, si elige categorías A y B, con A en rojo y B en azul, entonces los resultados se mostrarán en 
+                            una combinación de esos dos colores: púrpura.',
+	'OR' => 'OR',
+	'ANDHEADER' => 'AND',
+	'ANDBODY' =>			'El operador AND le permite ver los reportes que pertenecen a todas las categorías seleccionadas.<br/><br/>
+							Por ejemplo, si ha seleccionado categorías A, B, y C, entonces solamente verá los reportes que pertenezcan 
+                            a las tres categorías. <br/><br/>
+							Cuando se selecciona AND, los marcadores se pintarán de acuerdo a la categoría seleccionada.
+							Por ejemplo, si elige categorías A y B, con A en rojo y B en azul, entonces los resultados se mostrarán en 
+                            una combinación de esos dos colores: púrpura.',
+	'AND' => 'AND',
+
+// Kpetermeni's Entries
+	'header_info' => 'Este mapa muestra todos los reportes que Ud esta autorizado a ver, incluyendo aquellos que no han sido aprobados.',
+	'logical_operators' => 'Operadores Logicos',
+	'unapproved_reports' => 'Mostrar los reportes no aprobados en negro',
+	'status_filters' => 'Filtros',
+	'or' => 'OR',
+	'and' => 'AND',
+	'or_details' => 'Mostrar todos los reportes que pertenecen al menos a una de las categorias seleccionadas',
+	'and_details' => 'Mostrar los reportes que pertenecen a todas las categorias seleccionadas',
+	'show_all_reports' => 'Mostrar todos los reportes',
+	'embedd_html' => 'Enlace a este mapa',
+	//john's entries
+	'group_categories' => 'Categorias de grupo',
+	'site_categories' => 'Categorias del sitio'
+	
+
+);
+	
+	
+	
+	
+
